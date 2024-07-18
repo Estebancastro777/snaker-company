eb9b7f4e568c378be2308f72bd2ac8cd8b127ed7
Estebancastro777 <137116504+Estebancastro777@users.noreply.github.com>
2024-07-18 13:15:13 -0300

    terminar card

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,27 +1,41 @@
+import { useState } from "react"
 import logoSnakers from "@/assets/images/logo.svg"
+import imgSmall from "@/assets/images/image-product-1-thumbnail.jpg"
+
 import Avatar from "@/assets/images/image-avatar.png"
 import MenuIcon from "@/components/icons/MenuIcon"
 import CartIcon from "@/components/icons/CartIcon"
 import IconClose from "@/components/icons/IconClose"
-import { useState } from "react"
+import DeleteIcon from "@/components/icons/DeleteIcon"
 import NavHeader from "./NavHeader"
 
 
 const MainHeader = () => {
 
-    const [navClass, setNavClass] = useState("hidden md:mt-2 font-bold md:mr-auto md:block md:flex md:flex-row md:gap-4")
+    const [navClass, setNavClass] = useState("hidden md:mt-1 font-bold md:mr-auto md:block md:flex md:flex-row md:gap-4")
 
     const handleCloseMenu = () => {
         setNavClass("hidden font-bold md:static md:mt-2 md:mr-auto md:block md:flex md:flex-row md:p-0 md:gap-4")
     }
 
     const handleOpenMenu = () => {
-        setNavClass("flex flex-col z-10 absolute font-bold top-0 left-0 w-4/5 bg-white h-full p-8 md:mt-2 md:static md:mr-auto md:block md:flex md:flex-row md:p-0 md:gap-4")
+        setNavClass("flex flex-col z-20 absolute font-bold top-0 left-0 w-4/5 bg-white h-full p-8 md:mt-2 md:static md:mr-auto md:block md:flex md:flex-row md:p-0 md:gap-4")
     }
+
+    const [cardClass, setCardClass] = useState("hidden")
+
+    const handleCloseCard = () => {
+        setCardClass("hidden")
+    }
+
+    const handleOpenCard = () => {
+        setCardClass("rounded-lg w-[93%] md:w-auto top-[125%] md:top-0 left-0 md:left-auto mx-4 md:mx-0 z-10 absolute md:right-0 font-bold bg-white md:mt-20 md:px-4 md:pb-8 md:pt-2")
+    }
+
 
     return (
         <>
-            <header className="container mx-auto py-4 px-6 md:py-0 md:px-10 flex aling-center gap-8">
+            <header className="relative container mx-auto py-4 px-6 md:py-0 md:px-10 flex aling-center gap-8">
                 <button className="md:hidden " onClick={handleOpenMenu}>
                     <MenuIcon />
                 </button>
@@ -30,18 +44,40 @@ const MainHeader = () => {
                     <button className="md:hidden mb-10" onClick={handleCloseMenu}>
                         <IconClose />
                     </button>
-                    <NavHeader text="Collection"/>
-                    <NavHeader text="Men"/>
-                    <NavHeader text="Women"/>
-                    <NavHeader text="About"/>
-                    <NavHeader text="Contact"/>
+                    <NavHeader text="Collection" />
+                    <NavHeader text="Men" />
+                    <NavHeader text="Women" />
+                    <NavHeader text="About" />
+                    <NavHeader text="Contact" />
                 </nav>
                 <div className="flex">
-                    <button className="mr-3">
+                    <button onClick={handleOpenCard} className="mr-3">
                         <CartIcon />
                     </button>
                     <img className="my-auto h-10" src={Avatar} alt="" />
                 </div>
+                <section className={cardClass} >
+                    <div className="grid">
+                        <h4 className="md:mb-6 mb-4 pt-2 pl-4 md:p-0">Card</h4>
+                        <hr className="md:mb-4 mb-4" />
+                        <div className="grid grid-cols-4 pb-4 md:pb-0 md:h-4">
+                            <img src={imgSmall} 
+                            alt=""
+                            className="ml-4 md:ml-0 h-16 rounded-lg"
+                            />
+                            <div className="grid col-span-2">
+                                <h6 className="font-bold ">Fall Limited Edition Snakers</h6>                                
+                                <p>
+                                    <span className="font-normal">125.00 x 3</span> <span className="font-bold">375.00</span>
+                                </p>
+                            </div>
+                            <button className="ml-16">
+                                <DeleteIcon />
+                            </button>
+                        </div>
+                        <button className=" bg-orange-500 rounded-lg p-2 m-3 mt-0 md:mt-16 md:m-0">CheckOut</button>
+                    </div>
+                </section>
             </header>
             <hr className="hidden md:flex md:container md:mx-auto" />
         </>
--- src/components/products/col-img/MainImg.jsx
+++ src/components/products/col-img/MainImg.jsx
@@ -1,13 +1,14 @@
 import SlideProduct from "@/components/products/col-img/SlideProduct"
+import ModalProduct from "@/components/products/col-img/SlideProduct"
 import { useState } from "react"
 
 
 const MainImg = ({ ARRAY_IMGS, ARRAY_IMGS_SMALL }) => {
-    
+
     const [isOpenModal, setOpenModal] = useState(false)
 
     const handleOpenModal = () => {
-        if(window.innerWidth > 767) return setOpenModal(true)
+        if (window.innerWidth > 767) return setOpenModal(true)
     }
 
     const handleCloseModal = () => {
@@ -19,17 +20,20 @@ const MainImg = ({ ARRAY_IMGS, ARRAY_IMGS_SMALL }) => {
             <SlideProduct
                 ARRAY_IMGS={ARRAY_IMGS}
                 ARRAY_IMGS_SMALL={ARRAY_IMGS_SMALL}
-                className = "grid md:grid-cols-4 gap-4"
-                handleOpenModal = {handleOpenModal}
+                className="grid md:grid-cols-4 gap-4"
+                handleOpenModal={handleOpenModal}
             />
             {isOpenModal && (
-            <SlideProduct
-                ARRAY_IMGS={ARRAY_IMGS}
-                ARRAY_IMGS_SMALL={ARRAY_IMGS_SMALL}
-                isOpenModal={isOpenModal}
-                className="hidden md:grid md:grid-cols-4 gap-4 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-md"
-                handleCloseModal = {handleCloseModal}
-            />
+                <>
+                    <ModalProduct
+                        ARRAY_IMGS={ARRAY_IMGS}
+                        ARRAY_IMGS_SMALL={ARRAY_IMGS_SMALL}
+                        isOpenModal={isOpenModal}
+                        className="hidden md:z-10 md:grid md:grid-cols-4 gap-4 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-md"
+                        handleCloseModal={handleCloseModal}
+                    />
+                    <span className="fixed md:h-full md:w-full md:top-0 md:left-0 md:bg-black/70"></span>
+                </>
             )}
         </>
     )
